fbe55c4d6a630b83e853f9e2838711815c4474c6
YOUZYX <80650550+YOUZYX@users.noreply.github.com>
2025-04-30 04:05:01 +0100

    nad_contract_abi

---
+++ contract-abi/README.md
@@ -0,0 +1,10 @@
+# contract-abi
+## How to use abi?
+"contract-abi": "https://github.com/Naddotfun/contract-abi.git#v0.3.0",
+
+```
+add contract-abi to package.json
+npm install or
+yarn install or
+pnpm install
+```
---
+++ contract-abi/index.ts
@@ -0,0 +1,21 @@
+//bonding curve
+export {default as IBondingCurve} from "./IBondingCurve.json"
+export {default as IBondingCurveFactory} from "./IBondingCurveFactory.json"
+
+//core
+export {default as ICore} from "./ICore.json"
+
+//mint party
+// export {default as IMintParty} from "./IMintParty.json"
+// export {default as IMintPartyFactory} from "./IMintPartyFactory.json"
+
+//token
+export {default as IToken} from "./IToken.json"
+
+//lock
+export {default as ILock} from "./ILock.json"
+
+//uniswap v2
+export {default as IUniswapV2Factory} from "./IUniswapV2Factory.json"
+export {default as IUniswapV2Pair} from "./IUniswapV2Pair.json"
+export {default as IUniswapV2Router} from "./IUniswapV2Router.json"
